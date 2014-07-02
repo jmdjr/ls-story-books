790d00f9f7e7c163c0ba79c7bc6ee47e47ec8a18
jmdcodejunkie <jmdcodejunkie@gmail.com>
2014-07-02 09:13:54 +0000

    work on grid, got stuff running correctly so far, implementing the access directionals.

--- StoryBooks/StoryBooksSite/Scripts/require/JDGEGrid.js
+++ StoryBooks/StoryBooksSite/Scripts/require/JDGEGrid.js
@@ -10,14 +10,16 @@
     // initializes the simple grid object.
     //  grid is an array of objects, containing references to objects. 
     // if a mapper function is provided, the values provided in the grid will be mapped using the mapper function.
-    // Mapper function must accept at least one input, and return desired object.
+    // Mapper function must accept at least one input (Value to be stored), and return desired object.
+
     // maps data to a 2d grid, allowing for random access, and traversing via navigation based functions:
-    //  Unless otherwise noted, the following functions accept either an x/y coordinate or a tile object.
+    //  Unless otherwise noted, the following functions accept only a tile object.
     //    north, south, east, west, northeast, southeast, northwest, southwest:
     //          returns the object stored at tile in this direction of the provided coords or tile object.
     //    northTile, southTile, eastTile, westTile, northeastTile, southeastTile, 
     //         returns tile in the direction of the provided coords or tile object.
     p.initialize = function (grid, mapper) {
+        var $this = this;
         if(!$.isArray(grid))
         {
             $.error('JDGE: SimpleGrid: Initialization error 0000 - Grid is not formatted properly or does not exist.');
@@ -31,31 +33,95 @@
         this.height = grid.length;
 
         this.Tiles = [];
+
+        // writing mapper function to appropriate TileGeneration function
+
         if (!mapper) {
-            mapper = function (tileValue) { return new jdge.SimpleGridTile( };
+            mapper = function (tileValue, x, y) { return new jdge.SimpleGridTile(tileValue, x, y); };
+        }
+        else
+        {
+            var t_mapper = mapper;
+            mapper = function (tileValue, x, y) { return new jdge.SimpleGridTile(t_mapper(tileValue), x, y); };
         }
 
-        var i = 1;
-
-        while (i < this.height) {
-            if (!$.isArray(grid[i])) {
-                $.error('JDGE: SimpleGrid: Initialization error 0002 - Grid Row ' + i + ' is not formatted properly');
+        var w = h = 0;
+        while (h < this.height) {
+            if (!$.isArray(grid[h])) {
+                $.error('JDGE: SimpleGrid: Initialization error 0002 - Grid Row ' + h + ' is not formatted properly');
                 break;
             }
-            ++i;
+
+            this.Tiles.push([]);
+            w = 0;
+
+            while (w < this.width) {
+                this.Tiles[h].push(mapper(grid[h][w], w, h));
+                ++w;
+            }
+
+            ++h;
         }
+
+        this.printHtml = function () {
+            var divTable = '<div class="JDGEGrid-table">';
+            var columnHeaders = '<div class="JDGEGrid-row JDGEGrid-column-header-row"><div class="JDGEGrid-row-header JDGEGrid-cell"></div>';
+            var row = '';
+            var w = h = 0;
+            while (h < this.height) {
+                w = 0;
+                row += '<div class="JDGEGrid-row"> <div class="JDGEGrid-row-header JDGEGrid-cell">' + h + '</div>';
+                while (w < this.width) {
+                    row += '<div class="JDGEGrid-column JDGEGrid-cell">' + $this.Tiles[h][w].toString() + '</div>';
+
+                    if (h == 0) {
+                        columnHeaders += '<div class="JDGEGrid-column JDGEGrid-cell">' + w + '</div>';
+                    }
+                    ++w;
+                }
+
+                if (h == 0) {
+                    columnHeaders += '</div> <div style="clear:both;"></div>';
+                }   
+
+                row += '</div> <div style="clear:both;"></div>';
+                ++h;
+            }
+            divTable += columnHeaders + row + '</div>';
+
+            $('body').append(divTable);
+        }
+        this.isTile = function (object) { return object instanceof jdge.SimpleGridTile;  }
+        this.north = function (tile) {
+            if (!$this.isTile(tile) || tile.coords.) {
+                return null;
+            }
+
+            $this.Tiles
+        }
+
+
+
     }
 
     /*******************************************************************************************************************************/
     // a single tile in the Simple grid.  
     // contains a value and its coords in the grid, for reference. 
     jdge.SimpleGridTile = function (value, x, y) {
+        var $this = this;
+
         this.value = value;
 
-        if(x && y) {
+        if (typeof x !== 'undefined' & typeof y !== 'undefined') {
             this.coords = { x: x, y: y };
         }
 
+        this.toString = function () {
+            if (typeof $this.value !== 'undefined' && $this.value != null) {
+                return $this.value.toString();
+            }
+            return 'NDF';
+        };
 
     }
 
