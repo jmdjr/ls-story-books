2a084c1ddcf873887482cc80d1e0f8155f066b84
jmdcodejunkie@gmail.com <jmdcodejunkie@gmail.com@87e1df3a-c96b-e3e8-2e8b-7db9907a3b13>
2014-07-02 09:13:54 +0000

    work on grid, got stuff running correctly so far, implementing the access directionals.

--- trunk/StoryBooks/StoryBooksSite/Scripts/train/Game.js
+++ trunk/StoryBooks/StoryBooksSite/Scripts/train/Game.js
@@ -1,5 +1,9 @@
 ﻿define(['jquery', 'JDGEngine', 'JDGEGrid'], function ($) {
     $(function () {
-        var grid = new jdge.SimpleGrid([[0, 0, 0], 2]);
+        var grid = new jdge.SimpleGrid([
+            [0, 0, 0, null, 0],
+            [1, 2, 3]
+        ]);
+        grid.printHtml();
     });
 });
