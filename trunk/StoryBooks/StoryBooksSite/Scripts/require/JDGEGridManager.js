69b8d686e8e9228e43e5fdc42c9d6d5a856e4657
jmdcodejunkie@gmail.com <jmdcodejunkie@gmail.com@87e1df3a-c96b-e3e8-2e8b-7db9907a3b13>
2014-07-09 20:06:30 +0000

    added new images, fixing scripts.

--- trunk/StoryBooks/StoryBooksSite/Scripts/require/JDGEGridManager.js
+++ trunk/StoryBooks/StoryBooksSite/Scripts/require/JDGEGridManager.js
@@ -20,6 +20,7 @@ if (define) {
                 var $this = this;
 
                 this.Grids = [];
+                this.RenderedTiles = [];
                 this.Engine = Engine;
                 this.selected_grid_name = "";
 
@@ -27,7 +28,7 @@ if (define) {
                     this.Engine.addChild($this);
                 }
 
-
+                // A render pattern must accept a SimpleGridTileObject and return a DisplayObject which displays it's content.
                 this.RenderingPatterns = {
                     Square: function (tile) {
                         // height and width of each tile
@@ -37,19 +38,20 @@ if (define) {
                         tileContainer.height = height;
                         tileContainer.width = width;
 
-                        if (!jdge.IsUndefined(tile) && !jdge.IsUndefined(tile.value)) {
+                        if (!jdge.IsUndefined(tile)) {
 
-                            if (tile.value instanceof DisplayObject) {
+                            if (!jdge.IsUndefined(tile.value) && tile.value instanceof createjs.DisplayObject) {
                                 tileContainer.addChild(tile.value);
                             }
                             else {
-                                tileContainer.addChild(new Text(tile.toString()));
+                                tileContainer.addChild(new createjs.Text(tile.toString()));
                             }
+
+                            tileContainer.x = width * tile.coords.x;
+                            tileContainer.y = height * tile.coords.y;
                         }
 
-                        $this.addChild(tileContainer);
-                        tileContainer.x = width * tile.coords.x;
-                        tileContainer.y = height * tile.coords.y;
+                        return tileContainer;
                     }
                 }
 
@@ -67,40 +69,53 @@ if (define) {
                 this.selectGrid = function (name) {
                     if (!jdge.IsUndefined($this.Grids[name])) {
                         $this.selected_grid_name = name;
+                        $this.clearGridTiles();
                     }
+
                     return $this.selected_grid_name;
                 }
 
+                this.clearGridTiles = function () {
+                    var tile = this.RenderedTiles.pop();
+                    while (tile != undefined) {
+                        this.removeChild(tile);
+                        tile = this.RenderedTiles.pop();
+                    }
+                }
+
                 this.Draw = function (gridName, renderPattern) {
-
-                    // If the grid name is not provided in the draw function, and there has not been a selected grid name,
-                    //  select the first grid in the grid array.
-                    if (jdge.IsUndefined(gridName) && $this.selected_grid_name == "") {
-                        $this.selected_grid_name = $this.Grids[0];
-                    }
-                    else if (!jdge.IsUndefined(gridName)) {
-                        $this.selected_grid_name = gridName;
-                    }
-
-                    var grid = $this.Grids[$this.selected_grid_name];
-                    if(jdge.IsUndefined(grid))
-                    {
-                        $.error('JDGE: GridManager: Error 0000 - Drawing failed due to non-existant grid being referenced.');
-                    }
-
-                    var rendering = $this.RenderPattern;
-
-                    if (!jdge.IsUndefined(renderPattern) && typeof renderPattern === "function") {
-                        rendering = renderPattern;
-                    }
-
-                    var h = w = 0;
-                    while (h < grid.height) {
-                        while (w < grid.width) {
-                            rendering(grid.getTileAt(w, h));
-                            ++w;
+                    if ($this.RenderedTiles.length == 0) {
+                        // If the grid name is not provided in the draw function, and there has not been a selected grid name,
+                        //  select the first grid in the grid array.
+                        if (jdge.IsUndefined(gridName) && $this.selected_grid_name == "") {
+                            $this.selected_grid_name = $this.Grids[0];
                         }
-                        ++h;
+                        else if (!jdge.IsUndefined(gridName)) {
+                            $this.selected_grid_name = gridName;
+                        }
+
+                        var grid = $this.Grids[$this.selected_grid_name];
+                        if (jdge.IsUndefined(grid)) {
+                            $.error('JDGE: GridManager: Error 0000 - Drawing failed due to non-existant grid being referenced.');
+                        }
+
+                        var rendering = $this.RenderPattern;
+
+                        if (!jdge.IsUndefined(renderPattern) && typeof renderPattern === "function") {
+                            rendering = renderPattern;
+                        }
+
+                        var h = w = 0;
+                        while (h <= grid.height) {
+                            while (w <= grid.width) {
+                                var tile = rendering(grid.getTileAt(w, h));
+                                this.RenderedTiles.push(tile);
+                                this.addChild(tile);
+                                ++w;
+                            }
+                            w = 0;
+                            ++h;
+                        }
                     }
                 }
 
@@ -111,7 +126,6 @@ if (define) {
                     return $this;
                 }
             }
-
 
             scope.jdge = jdge;
         }(window));
